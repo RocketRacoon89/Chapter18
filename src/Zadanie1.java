1f646ebecc6ec2250ae875e99152dfcadea52c4d
Mike <paagrio2005@yandex.ru>
2022-01-17 03:13:42 +0300

    Zadanie1 and Zadanie2

--- src/Zadanie1.java
+++ src/Zadanie1.java
@@ -3,14 +3,13 @@ import java.util.Arrays;
 public class Zadanie1 {
     public static void main(String[] args) {
         int [] array = {3, 8, 15, 17};
-        //int k = Arrays.binarySearch(array, 15);
-        //System.out.println(k);
         Zadanie1 z = new Zadanie1();
-        System.out.println(z.getind(array, 23));
+        int [] otv = z.getind(array, 23);
+        for(int y:otv)
+            System.out.print(y +" ");
     }
 
     int [] getind(int [] a, int sum) {
-        if(a.length>0) {
             for (int i=0; i<a.length; i++) {
                 for (int y=0; y<a.length; y++) {
                     if(i!=y) {
@@ -19,15 +18,13 @@ public class Zadanie1 {
                             int [] ret = {i, y};
                             return ret;
                         };
-                        int [] ret = {0};
-                        return ret;
                     }
                 }
             }
-
-        }
         int [] ret = {0};
         return ret;
     }
 
 }
+//int k = Arrays.binarySearch(array, 15);
+//System.out.println(k);
