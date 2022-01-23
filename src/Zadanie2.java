4c431ae486293b7893c339fbbacdb6d85655d318
Mike <paagrio2005@yandex.ru>
2022-01-23 16:39:23 +0300

    zadanie2_fix0.1

--- src/Zadanie2.java
+++ src/Zadanie2.java
@@ -1,32 +1,34 @@
 public class Zadanie2 {
     public static void main(String[] args) {
-        int [] a = {4, 6, 5, 6, 6, 8, 6};
+        int[] a = {4, 6, 5, 6, 6, 8, 6};
         Zadanie2 z2 = new Zadanie2();
         z2.doub(a);
     }
 
-    boolean doub(int [] o) {
+    boolean doub(int[] o) {
         boolean res = false;
         int count = 1;
         int d = 0;
-        for(int i = 0; i<o.length; i++) {
+        for (int i = 0; i < o.length; i++) {
             System.out.println(i);
-            for(int j = i; j<o.length; j++) {
-                if(i!=j) {
-                    if(o[i]==o[j]) {
+            for (int j = 0; j < o.length; j++) {
+                if (i != j) {
+                    if (o[i] == o[j]) {
                         count++;
                         d = o[i];
                         res = true;
                     }
                 }
+
             }
-            if(res == true) {break;}
+            if(res) {break;}
+
         }
-        if (res){
-        System.out.println("Число "+d +" повторяется "+ count+" раз");
-        return res;
-        }
-        else
+
+        if (res) {
+            System.out.println("Число " + d + " повторяется " + count + " раз");
+            return res;
+        } else
             System.out.println("Совпадений нет");
         return res;
     }
